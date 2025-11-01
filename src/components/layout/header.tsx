042e0fe5136bb4a410928415aa9d2d4c635f67d4
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-11-01 18:13:05 +0000

    o botao acessar painel esta redirecionando para/painel e nao para /login

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,17 +5,8 @@ import Link from "next/link";
 import { Logo } from "../icons";
 import { Button } from "../ui/button";
 import { LogIn } from "lucide-react";
-import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
 
 export function Header() {
-    const pathname = usePathname();
-    
-    // Header não será exibido em nenhuma página logada que usa o AppLayout
-    if (pathname.startsWith('/setores') || pathname.startsWith('/motoristas') || pathname.startsWith('/veiculos') || pathname.startsWith('/viagens') || pathname.startsWith('/escalas') || pathname.startsWith('/relatorios') || pathname.startsWith('/perfil') || pathname.startsWith('/settings') || pathname.startsWith('/support') || pathname === '/login') {
-        return null;
-    }
-
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background">
             <div className="container flex h-14 items-center">
